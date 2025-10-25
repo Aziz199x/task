66870b49710ccd6b97140fbd8addd52b14dae5bb
[dyad] <hunter.ksa1993@gmail.com>
2025-10-25 23:01:43 +0300

    [dyad] wrote 1 file(s), deleted 2 file(s)

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Moon } from "lucide-react"
+import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
@@ -21,12 +21,8 @@ export function ThemeSwitcher() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm">
-          {/* Custom icon for light theme */}
-          <img 
-            src="/light-theme-icon.png" 
-            alt={t('light')} 
-            className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" 
-          />
+          {/* Use Sun icon for light theme */}
+          <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           {/* Moon icon for dark theme */}
           <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
