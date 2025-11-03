624e79035346a4ef48970fee15a2e92347323f02
[dyad] <hunter.ksa1993@gmail.com>
2025-11-03 21:58:00 +0300

    [dyad] wrote 1 file(s)

--- android/app/src/main/java/com/example/jadelobstersprint/MainActivity.java
+++ android/app/src/main/java/com/example/jadelobstersprint/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.jadelobstersprint;
 
 import com.getcapacitor.BridgeActivity;
-import com.capacitorjs.plugins.statusbar.StatusBar; // Import StatusBar
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(android.os.Bundle savedInstanceState) {
-        registerPlugin(StatusBar.class); // Register StatusBar plugin
         super.onCreate(savedInstanceState);
     }
 }
