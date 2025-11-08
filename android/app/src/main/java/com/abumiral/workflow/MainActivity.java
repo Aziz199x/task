512fd05edbed0cebcf7a1bd7ca26a7401f5b2fbc
[dyad] <hunter.ksa1993@gmail.com>
2025-11-08 06:39:05 +0300

    [dyad] Fixed Android status bar by removing conflicting native code and forcing a transparent background - wrote 1 file(s) + extra files edited outside of Dyad

--- android/app/src/main/java/com/abumiral/workflow/MainActivity.java
+++ android/app/src/main/java/com/abumiral/workflow/MainActivity.java
@@ -1,16 +1,5 @@
 package com.abumiral.workflow;
 
-import android.os.Bundle;
-import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Ensure status bar is properly configured
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-    }
-}
+public class MainActivity extends BridgeActivity {}
